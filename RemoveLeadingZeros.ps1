6e6ae2cd19b5061c6cd2d4ae8b4320a72b22aa8f
Bernt Anker <bernt.anker@me.com>
2016-03-18 12:37:52 +0100

    Removes leading zeros from filename
    Removes leading zeros from filename. Supports recursive traverse.

---
+++ RemoveLeadingZeros.ps1
@@ -0,0 +1,20 @@
+﻿$path = "c:\users\bernt\Testmappe\"
+$recursive = $true
+
+if ($recursive) {
+    $fileList = Get-ChildItem $path -File -Recurse
+} else {
+    $fileList = Get-ChildItem $path -File 
+}
+
+foreach ($file in $fileList) 
+{
+    $newname = $file.Name.TrimStart("0")
+    Rename-Item $file.FullName $newname 
+}
+
+
+    
+
+
+
